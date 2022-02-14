3d89158c60a7f8b9ecb452a067b39b614d296e57
mfarijan01 <muhamadfarijan30@gmail.com>
2022-02-14 09:02:30 +0700

    first commit

---
+++ app/src/main/java/com/example/mfarijan/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.mfarijan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseUser firebaseUser;
+    private TextView textName;
+    private Button btnLogout;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textName = findViewById(R.id.name);
+        btnLogout = findViewById(R.id.btn_logout);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null) {
+            textName.setText(firebaseUser.getDisplayName());
+        } else {
+            textName.setText("Login Gagal!");
+        }
+
+        btnLogout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            finish();
+        });
+    }
+}
